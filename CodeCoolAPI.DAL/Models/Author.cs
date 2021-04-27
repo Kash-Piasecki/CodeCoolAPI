a36063dcdd8f836911939f58d3ad84d3ef907666
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-27 09:52:18 +0200

    Create Author model

---
+++ CodeCoolAPI.DAL/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CodeCoolAPI.DAL.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Counter { get; set; }
+        public IEnumerable<Material> Materials { get; set; }
+    }
+}
