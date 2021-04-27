8ee821ad631d1cf4159fca1db9f7e93b951385fd
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-27 09:56:11 +0200

    Create Review model

---
+++ CodeCoolAPI.DAL/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeCoolAPI.DAL.Models
+{
+    public class Review : BaseEntity
+    {
+        public string TextBased { get; set; }
+        public int DigitBased { get; set; }
+        public Material Material { get; set; }
+        public int MaterialId { get; set; }
+    }
+}
